1b76cf3da3d1c362ab1a75ceea211ac241acaf8c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-13 21:49:08 +0530

    initial commits of java spring autocare service system

---
+++ src/test/java/lk/autocare/vehicle_service_system/VehicleServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.autocare.vehicle_service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
