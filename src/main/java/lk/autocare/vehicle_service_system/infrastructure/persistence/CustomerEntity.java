1fd64c00cc9e00c67be64a95e68817e748b22f12
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-14 19:46:58 +0530

    feat: create customer entity and assign realtionship

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/persistence/CustomerEntity.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/persistence/CustomerEntity.java
@@ -1,4 +1,33 @@
 package lk.autocare.vehicle_service_system.infrastructure.persistence;
 
-public class CutomerEntity {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @NotBlank(message = "Full name required")
+    private String customerName;
+    @NotBlank(message = "Phone number is required")
+    private String customerPhone;
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be empty")
+    private String customerEmail;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<VehicleEntity> vehicles;
 }
