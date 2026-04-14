c1685e574f86011501dac17a128e77c77c895cd2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-14 11:59:09 +0530

    feat: define vehicle domain model

--- src/main/java/lk/autocare/vehicle_service_system/domain/model/Vehicle.java
+++ src/main/java/lk/autocare/vehicle_service_system/domain/model/Vehicle.java
@@ -1,4 +1,24 @@
 package lk.autocare.vehicle_service_system.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicle {
+
+    private Long vehicleId;
+    private String vehicleNumber;
+    private String vehicleModel;
+    //enum
+    private VehicleFuelType vehicleFuelType;
+    private LocalDate lastServiceDate;
+    private LocalDate nextServiceDate;
+    private int mileage;
+    //customer object
+    private Customer customer;
 }
