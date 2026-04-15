6b28051fb9e9e850435d87b7950c24d56205a128
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-15 13:24:16 +0530

    feat: map shortinfo dto to customer web mapper to use in response dto

--- src/main/java/lk/autocare/vehicle_service_system/web/customer/webMappers/CustomerWebMapper.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/customer/webMappers/CustomerWebMapper.java
@@ -1,4 +1,21 @@
 package lk.autocare.vehicle_service_system.web.customer.webMappers;
 
+
+import lk.autocare.vehicle_service_system.domain.models.Customer;
+import lk.autocare.vehicle_service_system.web.customer.DTOs.CustomerRequestDTO;
+import lk.autocare.vehicle_service_system.web.customer.DTOs.CustomerResponseDTO;
+import lk.autocare.vehicle_service_system.web.vehicle.webMappers.VehicleWebMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {VehicleWebMapper.class})
 public interface CustomerWebMapper {
+
+    //RequestDTO to domain model
+    @Mapping(target = "customerId", ignore = true)
+    @Mapping(target = "vehicles", ignore = true)
+    Customer toDomainModel(CustomerRequestDTO customerRequestDTO);
+
+    //domain model to ResponseDTO
+    CustomerResponseDTO toResponseDTO(Customer customer);
 }
