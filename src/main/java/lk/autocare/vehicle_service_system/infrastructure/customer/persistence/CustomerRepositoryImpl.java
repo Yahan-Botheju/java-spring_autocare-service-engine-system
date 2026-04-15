67a9f02e1eaf24c876c8004a97fc62a467a26f8d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-15 11:07:01 +0530

    feat: config and inject jpa repo and persistence mapper

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/CustomerRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/CustomerRepositoryImpl.java
@@ -1,4 +1,18 @@
 package lk.autocare.vehicle_service_system.infrastructure.customer.persistence;
 
-public class CustomerRepositoryImpl {
+import lk.autocare.vehicle_service_system.domain.repositories.CustomerRepository;
+import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.jpa.JpaCustomerRepository;
+import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.mapper.CustomerPersistenceMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    //inject jpa repo
+    private final JpaCustomerRepository jpaCustomerRepository;
+
+    //inject customer persistence mappper
+    private final CustomerPersistenceMapper customerPersistenceMapper;
 }
