4fdee5d16b064f5ad754e68d05b49d84553848c9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-15 11:17:09 +0530

    feat: config customer controller

--- src/main/java/lk/autocare/vehicle_service_system/web/customer/controllers/CustomerController.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/customer/controllers/CustomerController.java
@@ -1,4 +1,19 @@
 package lk.autocare.vehicle_service_system.web.customer.controllers;
 
+import lk.autocare.vehicle_service_system.usecase.customer.CustomerUseCase;
+import lk.autocare.vehicle_service_system.web.customer.webMappers.CustomerWebMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/autocare/")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    //inject customer usecase
+    private final CustomerUseCase customerUseCase;
+
+    //inject web mapper
+    private final CustomerWebMapper customerWebMapper;
 }
