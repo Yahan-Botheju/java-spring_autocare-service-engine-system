4513a8805334b500bc2d797c110f40d7d02e829c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-15 21:58:50 +0530

    feat: remove ignore fields

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/mapper/CustomerPersistenceMapper.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/mapper/CustomerPersistenceMapper.java
@@ -3,7 +3,6 @@ package lk.autocare.vehicle_service_system.infrastructure.customer.persistence.m
 import lk.autocare.vehicle_service_system.domain.models.Customer;
 import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.entity.CustomerEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -16,6 +15,5 @@ public interface CustomerPersistenceMapper {
     Customer toDomainModel(CustomerEntity customerEntity);
 
     //domain model to entity
-    @Mapping(target = "customerId", source = "customerId")
     CustomerEntity toEntity(Customer customer);
 }
