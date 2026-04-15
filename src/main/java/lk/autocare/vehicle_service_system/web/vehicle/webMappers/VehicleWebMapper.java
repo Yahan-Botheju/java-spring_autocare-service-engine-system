b387acb2891e120def0d6351ba60bc5df09f1419
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-15 13:21:27 +0530

    feat: taken vehicle list data turn into dto object(domain model to dto)

--- src/main/java/lk/autocare/vehicle_service_system/web/vehicle/webMappers/VehicleWebMapper.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/vehicle/webMappers/VehicleWebMapper.java
@@ -1,4 +1,12 @@
 package lk.autocare.vehicle_service_system.web.vehicle.webMappers;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
+import lk.autocare.vehicle_service_system.web.vehicle.DTOs.VehicleShortInfoDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface VehicleWebMapper {
+
+    //vehicle model to vehicleShortIntoDTO
+    VehicleShortInfoDTO toVehicleShortInfoDTO(Vehicle vehicle);
 }
