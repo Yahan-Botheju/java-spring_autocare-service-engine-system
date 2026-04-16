2b47bc7fc8abd033c132e48a57d189d5e3d2a27b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-16 20:40:34 +0530

    feat: implement get all customer method in usecaseimpl

--- src/main/java/lk/autocare/vehicle_service_system/usecase/customer/CustomerUseCaseImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/usecase/customer/CustomerUseCaseImpl.java
@@ -1,11 +1,19 @@
 package lk.autocare.vehicle_service_system.usecase.customer;
 
+import lk.autocare.vehicle_service_system.domain.models.Customer;
 import lk.autocare.vehicle_service_system.domain.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CustomerUseCaseImpl implements  CustomerUseCase {
 
     //inject customer domain repo as initiating bean config
     private final CustomerRepository customerRepository;
+
+    //get all customers
+    public List<Customer> getAllCustomers(){
+        return customerRepository.getAllCustomers();
+    }
 }
