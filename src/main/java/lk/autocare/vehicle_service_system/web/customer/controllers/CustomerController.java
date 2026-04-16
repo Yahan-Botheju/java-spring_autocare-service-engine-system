a2174a2db7dd17b03025b41c679071073b53d6a9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-16 20:41:49 +0530

    feat: setup get all customer route/controller/web/customer

--- src/main/java/lk/autocare/vehicle_service_system/web/customer/controllers/CustomerController.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/customer/controllers/CustomerController.java
@@ -1,10 +1,15 @@
 package lk.autocare.vehicle_service_system.web.customer.controllers;
 
+import lk.autocare.vehicle_service_system.domain.models.Customer;
 import lk.autocare.vehicle_service_system.usecase.customer.CustomerUseCase;
+import lk.autocare.vehicle_service_system.web.customer.DTOs.CustomerResponseDTO;
 import lk.autocare.vehicle_service_system.web.customer.webMappers.CustomerWebMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/autocare/")
@@ -16,4 +21,11 @@ public class CustomerController {
 
     //inject web mapper
     private final CustomerWebMapper customerWebMapper;
+
+    @GetMapping("/all")
+    public List<CustomerResponseDTO> getAllCustomers(){
+        List<Customer> customerList = customerUseCase.getAllCustomers();
+        
+        return customerList.stream().map(e -> customerWebMapper.toResponseDTO(e)).toList();
+    }
 }
