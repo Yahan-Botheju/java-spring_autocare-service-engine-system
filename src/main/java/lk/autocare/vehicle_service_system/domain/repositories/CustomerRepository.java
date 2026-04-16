07c09204231acb47220e182a779410936596209e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-16 20:29:50 +0530

    feat: initiate get all customers method

--- src/main/java/lk/autocare/vehicle_service_system/domain/repositories/CustomerRepository.java
+++ src/main/java/lk/autocare/vehicle_service_system/domain/repositories/CustomerRepository.java
@@ -1,4 +1,11 @@
 package lk.autocare.vehicle_service_system.domain.repositories;
 
+import lk.autocare.vehicle_service_system.domain.models.Customer;
+
+import java.util.List;
+
 public interface CustomerRepository {
+
+    //get all customers
+    List<Customer> getAllCustomers();
 }
