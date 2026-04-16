47907d7ff7a7fad54ad5b8cc16a07a65505f6aa9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-16 20:39:11 +0530

    feat: initiate get all customers method usecase interface

--- src/main/java/lk/autocare/vehicle_service_system/usecase/customer/CustomerUseCase.java
+++ src/main/java/lk/autocare/vehicle_service_system/usecase/customer/CustomerUseCase.java
@@ -1,4 +1,11 @@
 package lk.autocare.vehicle_service_system.usecase.customer;
 
+import lk.autocare.vehicle_service_system.domain.models.Customer;
+
+import java.util.List;
+
 public interface CustomerUseCase {
+
+    //get all customers
+    List<Customer> getAllCustomers();
 }
