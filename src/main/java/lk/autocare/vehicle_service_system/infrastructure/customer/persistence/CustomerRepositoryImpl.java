182bf80556f47a579648815770f8702a538f2b44
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-16 20:31:17 +0530

    feat: get all customers and set to repo mapper to turn domain model

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/CustomerRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/persistence/CustomerRepositoryImpl.java
@@ -1,10 +1,13 @@
 package lk.autocare.vehicle_service_system.infrastructure.customer.persistence;
 
+import lk.autocare.vehicle_service_system.domain.models.Customer;
 import lk.autocare.vehicle_service_system.domain.repositories.CustomerRepository;
 import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.jpa.JpaCustomerRepository;
 import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.mapper.CustomerPersistenceMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @AllArgsConstructor
@@ -13,6 +16,14 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //inject jpa repo
     private final JpaCustomerRepository jpaCustomerRepository;
 
-    //inject customer persistence mappper
+    //inject customer persistence mapper
     private final CustomerPersistenceMapper customerPersistenceMapper;
+
+    //get all customers
+    @Override
+    public List<Customer> getAllCustomers(){
+        //get all from db, through mapper turn into domain model
+        return jpaCustomerRepository.findAll().stream()
+                .map(customerPersistenceMapper::toDomainModel).toList();
+    }
 }
