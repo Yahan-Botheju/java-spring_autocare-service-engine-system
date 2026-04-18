459f68ab338d64a090f3eead5694d0971b2d0aaf
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 19:48:06 +0530

    faet: setup find by id method

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
@@ -1,10 +1,13 @@
 package lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
 import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.jpa.JpaVehicleRepository;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.mapper.VehiclePersistenceMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,4 +18,10 @@ public class VehicleRepositoryImpl implements  VehicleRepository {
 
     //inject persistence mapper
     private final VehiclePersistenceMapper  vehiclePersistenceMapper;
+
+    //vehicle find by id
+    public Optional<Vehicle> findById(Long vehicleId){
+        return jpaVehicleRepository.findById(vehicleId)
+                .map(vehicle -> vehiclePersistenceMapper.toDomainModel(vehicle));
+    }
 }
