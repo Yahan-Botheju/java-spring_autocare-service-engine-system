bcb841b409e06363f3b9d8c07be59b44bf229542
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 11:53:01 +0530

    feat: initiate UseCaseImpl as bean

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
@@ -1,4 +1,17 @@
 package lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence;
 
+import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
+import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.mapper.VehiclePersistenceMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class VehicleRepositoryImpl {
+
+    //inject domain repo
+    private final VehicleRepository vehicleRepository;
+
+    //inject persistence mapper
+    private final VehiclePersistenceMapper  vehiclePersistenceMapper;
 }
