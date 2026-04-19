afe43d2cb60f82fed1d7ce11aa16955a271f5d9a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-19 17:51:02 +0530

    feat: setup get all vehicles route/controller/web/vehicles

--- src/main/java/lk/autocare/vehicle_service_system/web/vehicle/controllers/VehicleController.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/vehicle/controllers/VehicleController.java
@@ -1,4 +1,35 @@
 package lk.autocare.vehicle_service_system.web.vehicle.controllers;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
+import lk.autocare.vehicle_service_system.usecase.vehicle.VehicleUseCase;
+import lk.autocare.vehicle_service_system.web.vehicle.DTOs.VehicleResponseDTO;
+import lk.autocare.vehicle_service_system.web.vehicle.webMappers.VehicleWebMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/autocare/vehicles")
+@RequiredArgsConstructor
 public class VehicleController {
+
+    //inject use case
+    private final VehicleUseCase vehicleUseCase;
+
+    //inject web mapper
+    private final VehicleWebMapper vehicleWebMapper;
+
+    //get all vehicles
+    @GetMapping("/all")
+    public List<VehicleResponseDTO> allVehicles() {
+        //get all details as domain model
+        List<Vehicle> vehicles = vehicleUseCase.getAllVehicles();
+        //domain model to response dto
+        return vehicles.stream().map(e -> vehicleWebMapper.toResponseDTO(e)).toList();
+    }
+
+
 }
