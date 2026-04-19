9b4e2b036571d0b0d369e7c2a61f965ca600060d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-19 17:51:50 +0530

    fix: add getter setters

--- src/main/java/lk/autocare/vehicle_service_system/web/vehicle/DTOs/VehicleResponseDTO.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/vehicle/DTOs/VehicleResponseDTO.java
@@ -1,9 +1,15 @@
 package lk.autocare.vehicle_service_system.web.vehicle.DTOs;
 
 import lk.autocare.vehicle_service_system.domain.models.VehicleFuelType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleResponseDTO {
     private Long vehicleId;
     private String vehicleNumber;
