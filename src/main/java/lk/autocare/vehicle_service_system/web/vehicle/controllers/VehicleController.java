ef72b7e7fef432b076ef62992d5803ecf1e0bd6d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-19 21:43:57 +0530

    feat: setup register vehicle reponse route/controller/web/vehicle

--- src/main/java/lk/autocare/vehicle_service_system/web/vehicle/controllers/VehicleController.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/vehicle/controllers/VehicleController.java
@@ -2,12 +2,13 @@ package lk.autocare.vehicle_service_system.web.vehicle.controllers;
 
 import lk.autocare.vehicle_service_system.domain.models.Vehicle;
 import lk.autocare.vehicle_service_system.usecase.vehicle.VehicleUseCase;
+import lk.autocare.vehicle_service_system.web.vehicle.DTOs.VehicleRequestDTO;
 import lk.autocare.vehicle_service_system.web.vehicle.DTOs.VehicleResponseDTO;
 import lk.autocare.vehicle_service_system.web.vehicle.webMappers.VehicleWebMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +32,18 @@ public class VehicleController {
         return vehicles.stream().map(e -> vehicleWebMapper.toResponseDTO(e)).toList();
     }
 
+    //register vehicle
+    @PostMapping("/register")
+    public ResponseEntity<VehicleResponseDTO> saveVehicle(
+            @RequestBody VehicleRequestDTO vehicleRequestDTO
+            ){
+        //requestDTO to domain model
+        Vehicle vehicleDomainModel = vehicleWebMapper.toDomainModel(vehicleRequestDTO);
+
+        //get domain model and turn into responseDTO
+        VehicleResponseDTO response =  vehicleUseCase.saveVehicle(vehicleDomainModel);
+
+        return  ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
 }
