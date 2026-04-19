6af57b62e3dfb668440103c91a1f3e95862e42ce
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-19 17:46:33 +0530

    feat: setup getall vehicles method

--- src/main/java/lk/autocare/vehicle_service_system/usecase/vehicle/VehicleUseCaseImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/usecase/vehicle/VehicleUseCaseImpl.java
@@ -1,11 +1,19 @@
 package lk.autocare.vehicle_service_system.usecase.vehicle;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
 import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class VehicleUseCaseImpl implements  VehicleUseCase{
 
     //inject domain repo as bean this class
     private final VehicleRepository vehicleRepository;
+
+    //get all vehicle
+    public List<Vehicle> getAllVehicles(){
+        return vehicleRepository.getAllVehicles();
+    }
 }
