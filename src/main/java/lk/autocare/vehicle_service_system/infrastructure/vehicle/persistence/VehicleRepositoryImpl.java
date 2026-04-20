bfd0dd0b838774dc55fd38cf2095bb2e3d446a85
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-20 12:40:26 +0530

    feat: setup functionality of updated method

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
@@ -1,5 +1,6 @@
 package lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence;
 
+import lk.autocare.vehicle_service_system.GlobalExceptionHandler.ResourceNotFoundException;
 import lk.autocare.vehicle_service_system.domain.models.Vehicle;
 import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.entity.VehicleEntity;
@@ -22,12 +23,14 @@ public class VehicleRepositoryImpl implements  VehicleRepository {
     private final VehiclePersistenceMapper  vehiclePersistenceMapper;
 
     //vehicle find by id
+    @Override
     public Optional<Vehicle> findById(Long vehicleId){
         return jpaVehicleRepository.findById(vehicleId)
                 .map(vehicle -> vehiclePersistenceMapper.toDomainModel(vehicle));
     }
 
     //get all vehicle
+    @Override
     public List<Vehicle> getAllVehicles(){
         //find all vehicles from db then attached to list and return
         return jpaVehicleRepository.findAll().stream()
@@ -35,11 +38,30 @@ public class VehicleRepositoryImpl implements  VehicleRepository {
     }
 
     //create vehicle
+    @Override
     public Vehicle saveVehicle(Vehicle vehicle){
+        //turn domain model to entity
         VehicleEntity vehicleEntity = vehiclePersistenceMapper.toEntity(vehicle);
-
+        //save in db
         jpaVehicleRepository.save(vehicleEntity);
-
+        //return vehicle turning to domain model to mapper for response
         return  vehiclePersistenceMapper.toDomainModel(vehicleEntity);
+    }
+
+    //update vehicle details
+    @Override
+    public Vehicle updateVehicle(Long vehicleId, Vehicle vehicle){
+        //check vehicle exists
+        VehicleEntity vehicleEntity = jpaVehicleRepository.findById(vehicleId)
+                .orElseThrow(() -> new ResourceNotFoundException("Vehicle not found" + " "  + vehicleId));
+
+        //changed fields update with existing fields in entity using persistence mapper
+        VehicleEntity updatedEntity = vehiclePersistenceMapper.oldEntityDataToNewEntity(vehicle, vehicleEntity);
+
+        //save in db
+        jpaVehicleRepository.save(updatedEntity);
+
+        //updated entity turn into domain model and return for response
+        return vehiclePersistenceMapper.toDomainModel(updatedEntity);
     }
 }
