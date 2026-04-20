3cecb1ac13e052aff69d40a01035e1a3eca80ede
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-20 11:08:07 +0530

    feat: enum updated to lowercase to uppercase

--- src/main/java/lk/autocare/vehicle_service_system/domain/models/VehicleFuelType.java
+++ src/main/java/lk/autocare/vehicle_service_system/domain/models/VehicleFuelType.java
@@ -1,8 +1,16 @@
 package lk.autocare.vehicle_service_system.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum VehicleFuelType {
     PETROL,
     DIESEL,
     HYBRID,
-    ELECTRIC
+    ELECTRIC;
+
+    //turn lowercase to uppercase
+    @JsonCreator
+    public static VehicleFuelType toCapitalLetter(String value) {
+        return VehicleFuelType.valueOf(value.toUpperCase());
+    }
 }
