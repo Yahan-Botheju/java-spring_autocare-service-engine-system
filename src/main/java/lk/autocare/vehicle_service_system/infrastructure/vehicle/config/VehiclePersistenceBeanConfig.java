536cd7cb10baef57b25e73bae622674792983d28
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-20 11:17:59 +0530

    fix: change class name, VehiclePersistenceConfig to VehiclePersistenceBeanConfig

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/config/VehiclePersistenceBeanConfig.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/config/VehiclePersistenceBeanConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class VehiclePersistenceConfig {
+public class VehiclePersistenceBeanConfig {
     @Bean
     public VehicleRepository vehicleRepository(
             JpaVehicleRepository    jpaVehicleRepository,
