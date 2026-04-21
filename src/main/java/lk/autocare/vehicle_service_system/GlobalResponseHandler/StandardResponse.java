c3bdcfc5c35370468a1491ed2b0304ca544bdbec
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-21 10:25:57 +0530

    feat: implement standered response class

---
+++ src/main/java/lk/autocare/vehicle_service_system/GlobalResponseHandler/StandardResponse.java
@@ -0,0 +1,22 @@
+package lk.autocare.vehicle_service_system.GlobalResponseHandler;
+
+import lk.autocare.vehicle_service_system.web.customer.DTOs.CustomerResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StandardResponse<T> {
+    private int statusCode;
+    private String message;
+    private LocalDateTime timestamp;
+    private T data;
+
+
+}
