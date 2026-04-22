8401f7e39c93f246b7d79bcba4cd50547fe28ab2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-22 21:27:38 +0530

    feat: add past or present validation

--- src/main/java/lk/autocare/vehicle_service_system/web/vehicle/DTOs/VehicleRequestDTO.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/vehicle/DTOs/VehicleRequestDTO.java
@@ -3,6 +3,7 @@ package lk.autocare.vehicle_service_system.web.vehicle.DTOs;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lk.autocare.vehicle_service_system.domain.models.VehicleFuelType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class VehicleRequestDTO {
     @Min(value = 10, message = "At least vehicle should be 10 miles ")
     private int mileage;
     @NotNull(message = "Last service date cannot be empty")
+    @PastOrPresent(message = "Date should not be present")
     private LocalDate lastServiceDate;
     @NotNull(message = "Customer ID cannot be empty")
     private Long customerId;
