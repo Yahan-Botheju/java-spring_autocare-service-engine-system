8dbbcb9be48799eccef1cc216962b168737721e8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-24 10:54:45 +0530

    feat: create 409 exception class

--- src/main/java/lk/autocare/vehicle_service_system/GlobalExceptionHandler/ServiceAlreadyCompletedException.java
+++ src/main/java/lk/autocare/vehicle_service_system/GlobalExceptionHandler/ServiceAlreadyCompletedException.java
@@ -1,7 +1,7 @@
 package lk.autocare.vehicle_service_system.GlobalExceptionHandler;
 
 public class ServiceAlreadyCompletedException extends RuntimeException {
-  public ServiceAlreadyCompletedException(String message) {
-    super(message);
-  }
+    public ServiceAlreadyCompletedException(String message) {
+        super(message);
+    }
 }
